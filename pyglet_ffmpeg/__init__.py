0639895a35d888679b706fc3c066516494d0d420
Daniel <dan.gillet737@gmail.com>
2018-06-21 14:03:49 +0200

    chg: dev: Implement load_ffmpeg function

--- pyglet_ffmpeg/__init__.py
+++ pyglet_ffmpeg/__init__.py
@@ -1,5 +1,4 @@
 __version__ = '0.0.1.dev1'
 __title__ = 'pyglet_ffmpeg'
 
-def hello():
-    return 'World'
+from .loader import load_ffmpeg
--- tests/unit/test_hello.py
+++
@@ -1,6 +0,0 @@
-from pyglet_ffmpeg import hello
-
-
-def test_hello():
-    actual = hello()
-    assert 'World' == actual
